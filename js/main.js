236018d3a5ca41c099e629601d4c46f41532186c
William Wang <shadow25251@gmail.com>
2016-03-22 07:40:31 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c";
+
+function getData(cb) {
+  $.ajax({
+    url : API_PATH,
+    method : "GET",
+    success : function(data) {
+      cb(null, data);
+    },
+    error : function(err) {
+      cb(err);
+    }
+  });
+}
